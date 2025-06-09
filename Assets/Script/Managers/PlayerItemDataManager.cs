1f06abdc21c261aeb23274ab70210a6f66e75bac
RanKa110 <powerun44@gmail.com>
2025-06-09 16:26:33 +0900

    Add: 매니저 추가

---
+++ Assets/Script/Managers/PlayerItemDataManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerItemDataManager : MonoBehaviour
+{
+    
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
